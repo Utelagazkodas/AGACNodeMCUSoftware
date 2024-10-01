65684eb13c97675b6cd6ed6fd652de3549e9e552
Miglosz <vargamiki53@gmail.com>
2024-10-01 20:45:52 +0200

    works, kinda

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -3,15 +3,19 @@
 #include <WebSocketsClient.h>
 #include "HX711.h"
 #include "ArduinoJson.h"
+#include <string.h>
 
 // Replace with your network credentials
-const char *ssid = "miért nézed egyáltalán ezt?";
-const char *password = "nincs leak";
+const char *ssid = "HUAWEI-dr64";
+const char *password = "588Y293w";
 
 // WebSocket server details
-const char *websockets_server_host = "192.168.31.130";
-const uint16_t websockets_server_port = 12345; // Make sure your server is listening on this port
+const char *websockets_server_host = "192.168.100.141";
+const uint16_t websockets_server_port = 443; // Make sure your server is listening on this port
 const char *websockets_server_path = "/";    // WebSocket path if needed
+
+// Websocket server token
+const char *token = "token";
 
 // Create a WebSocket client object
 WebSocketsClient webSocket;
@@ -22,6 +26,12 @@ HX711 scale;
 int GCK_pin = D0;
 int DATA_pin = D2;
 
+int Ignite_pin = D1;
+
+int Thermo_pin = A0;
+// 0 if not launched
+unsigned long launched = 0;
+
 // Callback function to handle WebSocket events
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 {
@@ -29,27 +39,40 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     {
     case WStype_DISCONNECTED:
         Serial.println("WebSocket Disconnected");
+        launched = 0;
         break;
     case WStype_CONNECTED:
         Serial.println("WebSocket Connected");
-        webSocket.sendTXT("MBkV5Qz2PiymBoDWVoFc");
+        webSocket.sendTXT(token);
+        webSocket.sendTXT("ready");
 
         // Send a message when connected
         break;
     case WStype_TEXT:
-        Serial.printf("WebSocket message received: %s\n", payload);
+    
+        if(strcmp((char*)payload, "{\"entries\":[{\"table\":\"launched\",\"values\":[null]}]}") == 0){
+            Serial.println("launch");
+            launched = millis();
+            webSocket.sendTXT("launched");
+        }
         break;
     case WStype_BIN:
         Serial.println("WebSocket binary message received");
         break;
     case WStype_ERROR:
         Serial.println("Error");
+        launched = 0;
         break;
     }
 }
 
 void setup()
 {
+    pinMode(Thermo_pin, INPUT);
+
+    pinMode(Ignite_pin, OUTPUT);
+    digitalWrite(Ignite_pin, LOW);
+
     // Initialize serial communication
     Serial.begin(9600);
     delay(300);
@@ -67,7 +90,7 @@ void setup()
     // Initialize WebSocket
     webSocket.begin(websockets_server_host, websockets_server_port, websockets_server_path);
     webSocket.onEvent(webSocketEvent);
-            webSocket.sendTXT("MBkV5Qz2PiymBoDWVoFc");
+            webSocket.sendTXT(token);
 
 
     // Initialize scale
@@ -76,20 +99,30 @@ void setup()
     
 }
 
+unsigned long lastSend = 0;
+
+
+
 void loop()
 {
-    
+    if(launched == 0){
+    digitalWrite(Ignite_pin, LOW);
+    }
+    else{
+    digitalWrite(Ignite_pin, HIGH);
+    }
+
     // Run the WebSocket client
     webSocket.loop();
 
-    static unsigned long lastSend = 0;
-    if (millis() - lastSend > 700)
+    
+    if (millis() - lastSend > 700 && launched != 0)
     {
         lastSend = millis();
         
-        char buffer[46];
-        sprintf(buffer, "{\"table\":\"POWEEER\",\"value\":%ld}", scale.read());
-        Serial.println(buffer);
+        char buffer[128];
+        sprintf(buffer, "{\"entries\" : [{\"table\" : \"force\", \"value\" : %ld}, {\"table\" : \"timestamp\", \"value\" : %ld}, {\"table\" : \"temperature\", \"value\" : %ld}]}", scale.read(), millis() - launched, 0);
+        
         webSocket.sendTXT(buffer);
     }
 }
